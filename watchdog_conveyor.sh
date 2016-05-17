78a3cea94641c89aa3a3779ff99ec59fc6a974f0
ling0804 <525809432@qq.com>
2016-05-17 19:33:33 +0800

    fix the bug of watchdog script
    Change-Id: I7d206f906ac49ef8ae3e2e4323093cfe52096eb5

--- watchdog_conveyor.sh
+++ watchdog_conveyor.sh
@@ -5,7 +5,7 @@ do
     for bin in api clone resource ; do 
         if  ! pgrep -f conveyor-$bin >/dev/null ;
         then
-            (su openstack -c '/usr/bin/python /usr/bin/conveyor-$bin --config-file=/etc/conveyor/hybrid-v2v.conf > /dev/null 2>&1' &)
+            (su openstack -c "/usr/bin/python /usr/bin/conveyor-$bin --config-file=/etc/conveyor/conveyor.conf > /dev/null 2>&1" &)
             echo "restart conveyor-$bin...  $(date)"
         fi
     done
