410786e7825a49465e36ce5808c80a4864b96ebc
vklonghml <85885453@qq.com>
2017-05-25 11:45:05 +0800

    add partial test for clone manager
    Change-Id: I2f2aff8733a4f3462e48fc5c5337e69c16a30982

--- conveyor/tests/unit/clone/test_manager.py
+++ conveyor/tests/unit/clone/test_manager.py
@@ -13,13 +13,22 @@
 #    under the License.
 
 import mock
+import yaml
 
 from conveyor.clone import manager
+from conveyor.common import config
+from conveyor.conveyorheat.api import api
+from conveyor.db import api as db_api
 from conveyor.resource import api as resource_api
 from conveyor.tests import test
+from conveyor.tests.unit import fake_constants
 
 from conveyor import context
 from conveyor import exception
+from conveyor.resource import resource
+
+CONF = config.CONF
+# resource_from_dict = resource.Resource.from_dict
 
 
 class CloneManagerTestCase(test.TestCase):
@@ -29,11 +38,93 @@ class CloneManagerTestCase(test.TestCase):
         self.context = context.RequestContext('fake', 'fake', is_admin=False)
         self.clone_manager = manager.CloneManager()
 
+    def test_start_template_clone(self):
+        CONF.set_default('clone_migrate_type', 'cold')
+        self.clone_manager.heat_api.create_stack = mock.MagicMock()
+        self.clone_manager.heat_api.create_stack.return_value = \
+            fake_constants.FAKE_STACK
+        db_api.plan_stack_create = mock.MagicMock()
+        db_api.plan_stack_create.return_value = None
+        self.clone_manager.resource_api.update_plan = mock.MagicMock()
+        self.clone_manager.resource_api.update_plan.return_value = None
+        self.clone_manager.heat_api.events_list = mock.MagicMock()
+        self.clone_manager.heat_api.events_list.return_value = \
+            [api.Event(api.format_event(fake_constants.FAKE_EVENT_LIST))]
+        self.clone_manager.heat_api.get_resource = mock.MagicMock()
+        self.clone_manager.heat_api.get_resource.return_value = \
+            api.Resource(api.format_resource(fake_constants.FAKE_RESOURCE))
+        self.clone_manager.clone_managers.get('volume').\
+            start_template_clone = mock.MagicMock()
+        self.clone_manager.clone_managers.get('volume').\
+            start_template_clone.return_value = None
+        with test.nested(
+                mock.patch.object(
+                    self.clone_manager.heat_api, 'get_stack',
+                    return_value=api.Stack(
+                        api.format_stack(fake_constants.FAKE_STACK_STATUS))),
+                mock.patch.object(resource_api.ResourceAPI,
+                                  "get_plan_by_id")):
+            ret = self.clone_manager.start_template_clone(
+                self.context,
+                fake_constants.UPDATED_TEMPLATE)
+            self.assertEqual(fake_constants.FAKE_STACK['stack']['id'], ret)
+
+    def test_start_template_clone_error(self):
+        CONF.set_default('clone_migrate_type', 'cold')
+        self.clone_manager.heat_api.create_stack = mock.MagicMock()
+        self.clone_manager.heat_api.create_stack.side_effect = \
+            exception.PlanDeployError
+        self.assertRaises(exception.PlanDeployError,
+                          self.clone_manager.start_template_clone,
+                          self.context,
+                          fake_constants.UPDATED_TEMPLATE)
+
     @mock.patch.object(resource_api.ResourceAPI, "get_plan_by_id")
-    def test_export_clone_template(self, get_plan_by_id_mock):
+    def test_export_clone_template_error(self, get_plan_by_id_mock):
         get_plan_by_id_mock.return_value = None
         fake_id = 'fake-001'
         sys_clone = False
         self.assertRaises(exception.PlanNotFound,
                           self.clone_manager.export_clone_template,
                           self.context, fake_id, sys_clone)
+
+    @mock.patch.object(resource_api.ResourceAPI, "update_plan")
+    @mock.patch.object(resource_api.ResourceAPI, "get_plan_by_id")
+    def test_export_clone_template(self, mock1, mock2):
+        mock1.return_value = fake_constants.FAKE_PLAN
+        mock2.return_value = None
+        res = resource.Resource('test', 'test', 'test')
+        manager.resource_from_dict = mock.MagicMock()
+        manager.resource_from_dict.return_value = res
+        sys_clone = False
+        fake_id = 'fake-001'
+        self.clone_manager.clone_driver.handle_resources = mock.MagicMock()
+        self.clone_manager.clone_driver.handle_resources.return_value = None
+        yaml.safe_dump = mock.MagicMock()
+        yaml.safe_dump.return_value = None
+        ret = ({'stack_0': res}, '2017-05-22 02:01:59.136794')
+        with mock.patch("__builtin__.open", create=True) as mock_open:
+            mock_open.side_effect = \
+                mock.mock_open(read_data="Data1").return_value
+            self.assertEqual(
+                ret,
+                self.clone_manager.export_clone_template(self.context,
+                                                         fake_id, sys_clone))
+
+    def test_clone(self):
+        pass
+
+    def test_clone_error(self):
+        pass
+
+    def test_migrate(self):
+        pass
+
+    def test_migrate_error(self):
+        pass
+
+    def test_download_template(self):
+        pass
+
+    def test_download_template_error(self):
+        pass
--- conveyor/tests/unit/fake_constants.py
+++ conveyor/tests/unit/fake_constants.py
@@ -12,6 +12,222 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import datetime
+
 PROJECT_ID = '89afd400-b646-4bbc-b12b-c0a4d63e5bd3'
 USER_ID = 'c853ca26-e8ea-4797-8a52-ee124a013d0e'
 PLAN_ID = '1e5177e7-95e5-4a0f-b170-e45f4b469f6a'
+
+FAKE_PLAN = {
+    'plan_status': 'initiating',
+    'project_id': 'd23b65e027f9461ebe900916c0412ade',
+    'task_status': '',
+    'user_id': '7e11a86b90a14dfd96d4142c6ea1e539',
+    'original_resources': {
+        'stack_0': {
+            'name': 'stack_0',
+            'parameters': {},
+            'extra_properties': {
+                'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe'
+            },
+            'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe',
+            'type': 'OS::Heat::Stack',
+            'properties': {
+                'stack_name': 'test-stack',
+                'disable_rollback': True,
+                'parameters': {
+                    'OS::project_id': 'd23b65e027f9461ebe900916c0412ade',
+                    'image': '19a7e4c8-baa5-442a-859e-7e968dc8b189',
+                    'private_network_id':
+                        '01329398-2050-4152-a034-c1b302e70619',
+                    'flavor': '2',
+                    'OS::stack_name': 'test-stack'
+                },
+                'template': '{"heat_template_version": "2013-05-23",'
+                            ' "description": "A recorded server",'
+                            ' "parameters": {"image": {"default": '
+                            '"19a7e4c8-baa5-442a-859e-7e968dc8b189", '
+                            '"type": "string"}, "private_network_id": '
+                            '{"default": '
+                            '"01329398-2050-4152-a034-c1b302e70619", '
+                            '"type": "string"}, "flavor": {"default": "2",'
+                            ' "type": "string"}}, "resources": {"server":'
+                            ' {"type": "OS::Nova::Server", "properties": '
+                            '{"flavor": {"get_param": "flavor"}, "networks":'
+                            ' [{"network": {"get_param": '
+                            '"private_network_id"}}], "image": {"get_param":'
+                            ' "image"}, "availability_zone": '
+                            '"az01.dc1--fusionsphere"}}}}'
+            }
+        }
+    }, 'deleted': False, 'original_dependencies': {
+        'stack_0': {
+            'name_in_template': 'stack_0',
+            'dependencies': [],
+            'type': 'OS::Heat::Stack',
+            'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe',
+            'name': ''
+        }
+    }, 'plan_type': 'clone', 'updated_at': None,
+    'stack_id': None, 'updated_resources': {
+        'stack_0': {
+            'name': 'stack_0',
+            'parameters': {},
+            'extra_properties': {
+                'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe'
+            },
+            'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe',
+            'type': 'OS::Heat::Stack',
+            'properties': {
+                'stack_name': 'test-stack',
+                'disable_rollback': True,
+                'template': '{"heat_template_version": "2013-05-23", '
+                            '"description": "A recorded server", '
+                            '"parameters": {"image": {"default": '
+                            '"19a7e4c8-baa5-442a-859e-7e968dc8b189", '
+                            '"type": "string"}, "private_network_id": '
+                            '{"default": '
+                            '"01329398-2050-4152-a034-c1b302e70619", '
+                            '"type": "string"}, "flavor": {"default": "2", '
+                            '"type": "string"}}, "resources": {"server": '
+                            '{"type": "OS::Nova::Server", "properties": '
+                            '{"flavor": {"get_param": "flavor"}, "networks": '
+                            '[{"network": {"get_param": '
+                            '"private_network_id"}}], "image": {"get_param":'
+                            ' "image"}, "availability_zone": '
+                            '"az01.dc1--fusionsphere"}}}}',
+                'parameters': {
+                    'OS::project_id': 'd23b65e027f9461ebe900916c0412ade',
+                    'image': '19a7e4c8-baa5-442a-859e-7e968dc8b189',
+                    'private_network_id':
+                        '01329398-2050-4152-a034-c1b302e70619',
+                    'flavor': '2',
+                    'OS::stack_name': 'test-stack'
+                }
+            }
+        }
+    }, 'updated_dependencies': {
+        'stack_0': {
+            'name_in_template': 'stack_0',
+            'dependencies': [],
+            'type': 'OS::Heat::Stack',
+            'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe',
+            'name': ''
+        }
+    },
+    'plan_id': 'b3bd135b-2465-48b0-958c-fbcf53d75727',
+    'deleted_at': None, 'plan_name': 'b3bd135b-2465-48b0-958c-fbcf53d75727',
+    'created_at': '2017-05-22 01:01:59.136550',
+    'expire_at': '2017-05-22 02:01:59.136794'
+}
+
+FAKE_RETURN_RES = {
+    'stack_0': {
+        'name': 'stack_0',
+        'parameters': {},
+        'extra_properties': {
+            'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe'
+        },
+        'id': 'e76f3947-d76f-4dca-a5f4-5af9b57becfe',
+        'type': 'OS::Heat::Stack',
+        'properties': {
+            'stack_name': 'test-stack',
+            'disable_rollback': True,
+            'template': '{"heat_template_version": "2013-05-23", '
+            '"description": "A recorded server", '
+            '"parameters": {"image": {"default": '
+            '"19a7e4c8-baa5-442a-859e-7e968dc8b189", '
+            '"type": "string"}, "private_network_id": '
+            '{"default": '
+            '"01329398-2050-4152-a034-c1b302e70619", '
+            '"type": "string"}, "flavor": {"default": "2", '
+            '"type": "string"}}, "resources": {"server": '
+            '{"type": "OS::Nova::Server", "properties": '
+            '{"flavor": {"get_param": "flavor"}, "networks": '
+            '[{"network": {"get_param": '
+            '"private_network_id"}}], "image": {"get_param":'
+            ' "image"}, "availability_zone": '
+            '"az01.dc1--fusionsphere"}}}}',
+            'parameters': {
+                'OS::project_id': 'd23b65e027f9461ebe900916c0412ade',
+                'image': '19a7e4c8-baa5-442a-859e-7e968dc8b189',
+                'private_network_id': '01329398-2050-4152-a034-c1b302e70619',
+                'flavor': '2',
+                'OS::stack_name': 'test-stack'
+            }
+        }
+    }
+}
+
+ORI_TEMPLATE = {
+    'heat_template_version': datetime.date(2013, 5, 23),
+    'description': 'Generated template',
+    'parameters': {},
+    'expire_time': '2017-05-23 08:58:17.663434',
+    'plan_type': 'clone',
+    'resources': {
+        'volume_0': {
+            'type': 'OS::Cinder::Volume',
+            'properties': {
+                'availability_zone': 'az02.dc1',
+                'metadata': {
+                    'readonly': 'False'
+                },
+                'name': 'vol_02',
+                'size': 1
+            },
+            'extra_properties': {
+                'status': 'in-use',
+                'guest_format': 'ext3',
+                'mount_point': '/opt/8e330083-d795-4cef-86de-f69a7dd9076a',
+                'gw_id': 'e1cd1e68-d250-4924-8ef2-fd34c72dc696',
+                'is_deacidized': True,
+                'sys_dev_name': '/dev/vdc',
+                'gw_url': '162.3.140.236:9998',
+                'id': '8e330083-d795-4cef-86de-f69a7dd9076a'
+            }
+        }
+    }
+}
+
+UPDATED_TEMPLATE = {
+    'plan_id': '6052d9db-0b54-4f45-b5fe-62a62ac938eb',
+    'template': {
+        'heat_template_version': '2013-05-23',
+        'description': 'clone template',
+        'parameters': {},
+        'resources': {
+            'volume_0': {
+                'type': 'OS::Cinder::Volume',
+                'properties': {
+                    'size': 1,
+                    'availability_zone': 'az02.dc1',
+                    'name': 'vol_02',
+                    'metadata': {
+                        'readonly': 'False'
+                    }
+                },
+                'extra_properties': {
+                    'status': 'in-use',
+                    'guest_format': 'ext3',
+                    'mount_point':
+                        '/opt/8e330083-d795-4cef-86de-f69a7dd9076a',
+                    'gw_id': 'e1cd1e68-d250-4924-8ef2-fd34c72dc696',
+                    'is_deacidized': True,
+                    'sys_dev_name': '/dev/vdc',
+                    'gw_url': '162.3.140.236:9998',
+                    'id': '8e330083-d795-4cef-86de-f69a7dd9076a'
+                }
+            }
+        }
+    }
+}
+
+FAKE_STACK = {'stack': {'id': 'dd4a5239-2780-4104-aa46-8a8f3d2f717b'}}
+FAKE_STACK_STATUS = {'stack': {'id': 'dd4a5239-2780-4104-aa46-8a8f3d2f717b'},
+                     'stack_status': 'COMPLETE',
+                     'stack_action': 'CREATE'}
+FAKE_EVENT_LIST = {'resource_name': 'volume_0',
+                   'resource_status': 'COMPLETE'}
+FAKE_RESOURCE = \
+    {'physical_resource_id': '8e330083-d795-4cef-86de-f69a7dd9076a'}
